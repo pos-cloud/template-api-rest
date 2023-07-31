c6c84db02efc7b4fa5f0689ca2ac5fdfcf55815e
Franco Androetto <androettofranco@gmail.com>
2023-07-31 20:25:21 -0300

    feat devtools: add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    rules: {
+      'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf', 'revert']],
+      'scope-empty': [0], // Permite un alcance vacío (opcional)
+      'subject-empty': [2, 'never'], // El mensaje debe tener una descripción después de los dos puntos
+      'subject-full-stop': [2, 'never'], // El mensaje no debe terminar con un punto
+      'subject-case': [0], // No se aplica reglas de capitalización en la descripción
+    },
+  };
+  
